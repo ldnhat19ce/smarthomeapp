c64e7ccae642dbef874cd392bebb6ef61686885d
leducnhat <ldnhat.19ce@vku.udn.vn>
2023-09-30 09:02:22 +0700

    add ble activity

---
+++ app/src/main/java/com/ldnhat/smarthomeapp/common/constants/CommonConstant.kt
@@ -0,0 +1,16 @@
+package com.ldnhat.smarthomeapp.common.constants
+
+class CommonConstant {
+    companion object {
+        const val DATA_PLANE_ACTION = "data-plane"
+        const val TAG = "gatt-service"
+        const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+        const val LOCATION_PERMISSION_REQUEST_CODE = 2
+        const val BLUETOOTH_ALL_PERMISSIONS_REQUEST_CODE = 3
+        const val SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+        const val CHAR_FOR_READ_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+        const val CHAR_FOR_WRITE_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+        const val CHAR_FOR_INDICATE_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+        const val CCC_DESCRIPTOR_UUID = "00002902-0000-1000-8000-00805f9b34fb"
+    }
+}
---
+++ app/src/main/java/com/ldnhat/smarthomeapp/common/enumeration/BLELifecycleState.kt
@@ -0,0 +1,10 @@
+package com.ldnhat.smarthomeapp.common.enumeration
+
+enum class BLELifecycleState {
+    Disconnected,
+    Scanning,
+    Connecting,
+    ConnectedDiscovering,
+    ConnectedSubscribing,
+    Connected
+}
